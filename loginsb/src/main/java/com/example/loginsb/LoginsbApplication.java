f0ac14824ca426efd5f3047abd8f5c6c63f32467
antt1005 <vuan090603@gmail.com>
2024-09-13 14:23:11 +0700

    login

---
+++ loginsb/src/main/java/com/example/loginsb/LoginsbApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginsbApplication.class, args);
+	}
+
+}
---
+++ loginsb/src/test/java/com/example/loginsb/LoginsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
